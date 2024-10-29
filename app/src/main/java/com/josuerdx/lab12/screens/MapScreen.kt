81a68bddb038a780326bc950502543b1b322d4e9
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-10-28 19:17:25 -0500

    feat: Integrar selector de mapa en MapScreen

--- app/src/main/java/com/josuerdx/lab12/screens/MapScreen.kt
+++ app/src/main/java/com/josuerdx/lab12/screens/MapScreen.kt
@@ -4,14 +4,19 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.josuerdx.lab12.components.MapMarkers
 import com.josuerdx.lab12.components.MapPolygons
 import com.josuerdx.lab12.components.MapPolylines
+import com.josuerdx.lab12.components.MapTypeSelector
 
 @Composable
 fun MapScreen() {
@@ -20,11 +25,17 @@ fun MapScreen() {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
     }
 
+    // Estado para el tipo de mapa
+    val mapTypeState = remember { mutableStateOf(MapType.NORMAL) }
+
+    val mapProperties = MapProperties(mapType = mapTypeState.value)
+
     Box(modifier = Modifier.fillMaxSize()) {
-        // Añadir GoogleMap al layout
+        // Añadir GoogleMap al layout con las propiedades del mapa
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
+            cameraPositionState = cameraPositionState,
+            properties = mapProperties
         ) {
             // Añadir marcadores
             MapMarkers(ArequipaLocation)
@@ -35,6 +46,11 @@ fun MapScreen() {
             // Dibujar polilíneas
             MapPolylines()
         }
+
+        // Selector de tipo de mapa
+        MapTypeSelector(onMapTypeChange = { newType ->
+            mapTypeState.value = newType // Actualiza el tipo de mapa
+        })
     }
 
     // Controlar la cámara programáticamente
